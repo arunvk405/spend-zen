93a68d3ab059ff87115c63e2de1102b150b114ee
arun.vadakkekara@waypoint-systems.com <arun.vadakkekara@waypoint-systems.com>
2026-02-17 13:05:41 +0530

    url change

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -69,12 +69,15 @@ export default function Settings() {
 2. Tap the three dots menu (⋮).
 3. Tap 'Install App' or 'Add to Home Screen'.
 
-🔗 Link: https://spend-zen.netlify.app`;
+🔗 Link: https://spend-zen.netlify.app 
+🔗 Link: https://spend-zen-five.vercel.app/`;
+
 
         try {
             await Share.share({
                 message: message,
-                url: 'https://spend-zen.netlify.app',
+                // url: 'https://spend-zen.netlify.app',
+                url: 'https://spend-zen-five.vercel.app',
                 title: 'Join me on Spend Zen!'
             });
         } catch (error) {
