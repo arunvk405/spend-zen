a5d8cf7a5a7ffdc05bb37513ff1284179bf7abdb
arun.vadakkekara@waypoint-systems.com <arun.vadakkekara@waypoint-systems.com>
2026-03-04 17:00:36 +0530

    push

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -21,9 +21,11 @@ function RootLayoutNav() {
                         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
                         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover" />
                         <meta name="theme-color" content={Colors.background} />
+                        <meta name="mobile-web-app-capable" content="yes" />
+                        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
                     </Head>
                 )}
-                <StatusBar style={Colors.isDark ? "light" : "dark"} />
+                <StatusBar style={Colors.isDark ? "light" : "dark"} backgroundColor={Colors.background} translucent={true} />
                 <Stack
                     screenOptions={{
                         headerStyle: {
